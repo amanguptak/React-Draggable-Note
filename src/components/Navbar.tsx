af2cb8d7ea5bbe0faf2e77c28af0e7970c45655f
Aman Gupta <eagleax7@gmail.com>
2024-07-26 20:03:41 +0530

    nav-bar-routes-fixed

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -9,9 +9,9 @@ const Navbar = () => {
       </div>
       <div className="flex space-x-5 mx-3">
         <Link to="/" className="text-gray-800 hover:text-blue-600">Home</Link>
-        <Link to="/about" className="text-gray-800 hover:text-blue-600">About</Link>
+      
         <Link to="/add-note" className="text-gray-800 hover:text-blue-600">AddNote</Link>
-        <Link to="/contact" className="text-gray-800 hover:text-blue-600">Contact</Link>
+       
       </div>
     </div>
   );
