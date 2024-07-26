84e2001f4ea45a50de4430e85dd216b5b9becfac
Aman Gupta <eagleax7@gmail.com>
2024-07-26 19:44:06 +0530

    title changed

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,7 +5,7 @@ const Navbar = () => {
   return (
     <div className="p-2 py-3 flex justify-between items-center shadow-xl rounded-md bg-white">
       <div className="logo">
-      <p className='font-bold text-slate-800 mx-4'>Draggable Docker AWS EC2 </p>
+      <p className='font-bold text-slate-800 mx-4'>Draggable Docker  EC2 with CI/CD </p>
       </div>
       <div className="flex space-x-5 mx-3">
         <Link to="/" className="text-gray-800 hover:text-blue-600">Home</Link>
